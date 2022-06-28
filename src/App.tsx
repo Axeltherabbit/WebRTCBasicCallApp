41adc50735f2f4b2299d58a1cabafbf197308f01
Pietro Convalle <pietro.convalle@answerdigital.com>
2022-06-28 14:59:05 +0100

    created basic signal server and basic wrtc structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,50 @@ import { useEffect, useRef, useState } from 'react';
 import 'style/App.scss';
 import { LocalVideo } from './components/LocalVideo/LocalVideo';
 
-import { getDevicesCount } from './utilities/webrtc';
+import { getDevicesCount, SetUpConnection } from './utilities/webrtc';
 
 function App() {
   const myVideo = useRef<HTMLVideoElement>(null!);
+  const [deviceCounter, setDeviceCounter] = useState(0);
+  const [streamData, setStreamData] = useState<MediaStream | undefined>(undefined);
+  const [username, setUsername] = useState('');
+  const [connection, setConnection] = useState<WebSocket>();
+
   useEffect(() => {
+    getDevicesCount().then((n) => setDeviceCounter(n));
     navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
       myVideo.current.srcObject = stream;
+      setStreamData(stream);
     });
   }, []);
 
+  const login = (event) => {
+    event.preventDefault();
+    setConnection(SetUpConnection(username));
+  };
+
   return (
     <div className='App row'>
-      <p> device count : {}</p>
+      <p> device count : {deviceCounter}</p>
+      <p> Stream : {streamData?.active ? 'active' : 'not active'}</p>
+      <p> ID : {streamData?.id ?? undefined}</p>
+      {connection ? <p> Username : {username}</p> : null}
       <div className='column'>
-        <LocalVideo ref={myVideo} />
+        <LocalVideo videoRef={myVideo} />
       </div>
+      <label htmlFor='inputUsername'>Username :</label>
+      <form onSubmit={login}>
+        <input
+          id='inputUsername'
+          type='text'
+          onChange={(e) => setUsername(e.target.value)}
+          minLength={4}
+          title='Only letters allowed'
+          pattern='[A-Za-z]+'
+          required
+        />
+        <button type='submit'>Connect</button>
+      </form>
     </div>
   );
 }
--- src/components/LocalVideo/LocalVideo.tsx
+++ src/components/LocalVideo/LocalVideo.tsx
@@ -1,5 +1,5 @@
 import { LegacyRef, useEffect, useRef } from 'react';
 
-export const LocalVideo: React.FC<{ refVideo: LegacyRef<HTMLVideoElement> }> = ({ refVideo }) => {
-  return <video width='320' height='240' muted ref={refVideo} autoPlay playsInline />;
+export const LocalVideo: React.FC<{ videoRef: LegacyRef<HTMLVideoElement> }> = ({ videoRef }) => {
+  return <video width='320' height='240' muted ref={videoRef} autoPlay playsInline />;
 };
